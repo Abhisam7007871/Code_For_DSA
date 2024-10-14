b32b334eca3e3d1e8e77cdf6fa20eacbc541316e
Abhishek <abhisam7007871@gmail.com>
2024-10-14 23:59:05 +0530

    Initial Commit

--- DSA_CODE/ReversString.java
+++ DSA_CODE/ReversString.java
@@ -1,2 +1,22 @@
+
+import java.awt.datatransfer.SystemFlavorMap;
+import java.sql.SQLOutput;
+import java.util.*;
+
 public class ReversString {
+    // input: s = "abc"
+    //output: s = "cba"
+    // input: s = "This is Code"
+    //output: s = "edoc si siht"
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String nstr = "";
+        char ch;
+
+        System.out.println("Original String: " + str);
+
+
+    }
 }
