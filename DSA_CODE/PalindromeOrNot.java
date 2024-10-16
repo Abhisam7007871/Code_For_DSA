1fd47b6988812b068cc15f5c025e46668630f4a5
Abhishek <abhisam7007871@gmail.com>
2024-10-16 21:11:23 +0530

    Added PalindromeOrNot

---
+++ DSA_CODE/PalindromeOrNot.java
@@ -0,0 +1,38 @@
+//        Input: N = 12321
+//        Output: Yes
+//        Explanation: 12321 is a Palindrome number because after reversing its digits, the number becomes 12321 which is same as the original number.
+//        Input: N = 1234
+//        Output: No
+//        Explanation: 1234 is not a Palindrome number because after reversing its digits, the number becomes 4321 which is different from the original number.
+//
+
+import java.util.Scanner;
+
+public class PalindromeOrNot {
+    public static boolean checkPalindrome(int n)
+    {
+        int reverse = 0;
+
+        int temp = n;
+        while (temp != 0) {
+            reverse = (reverse * 10) + (temp % 10);
+            temp = temp / 10;
+        }
+
+        return (reverse == n);
+    }
+
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if (checkPalindrome(n)) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
