70fd72820b40c098547bf79676fbe78084410315
Abhishek <abhisam7007871@gmail.com>
2024-10-26 15:40:31 +0530

    Added Reverse_Array

--- DSA_CODE/Reverse_Array.java
+++ DSA_CODE/Reverse_Array.java
@@ -2,16 +2,32 @@ import java.util.Scanner;
 
 public class Reverse_Array {
 
+//    Input : 1, 2, 3, 4, 5
+//    Output :5, 4, 3, 2, 1
+//
+//    Input :  10, 20, 30, 40
+//    Output : 40, 30, 20, 10
 
 
+    static void reverse(int a[], int n)
+    {
+        int[] b = new int[n];
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = a[i];
+            j = j - 1;
+        }
+
+        // printing the reversed array
+        System.out.println("Reversed array is: \n");
+        for (int k = 0; k < n; k++) {
+            System.out.println(b[k]);
+        }
+    }
 
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int[] arr = sc.nextInt();
-
+        int [] arr = {10, 20, 30, 40, 50};
+        reverse(arr, arr.length);
     }
 }
